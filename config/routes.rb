8e75084fabca971fbf2395513d90da2dbea8c5ad
maebeale <maebeale@gmail.com>
2018-06-08 14:19:16 -0400

    Merge conflict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
   root 'projects#index'
   resources :projects
->>>>>>> master
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
