f0400cfc471996721d597a8433b76c0686f3c703
Brian Murphy <bmurphy1@mac.com>
2018-06-08 13:58:01 -0400

    Initial Project and Target seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_143501) do
+ActiveRecord::Schema.define(version: 2018_06_08_174840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "targets", force: :cascade do |t|
+    t.string "target_type"
+    t.text "description"
+    t.string "name"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_targets_on_project_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,23 +47,6 @@ ActiveRecord::Schema.define(version: 2018_06_08_143501) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "targets", force: :cascade do |t|
-    t.string "type"
-    t.text "description"
-    t.string "name"
-    t.bigint "project_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_targets_on_project_id"
   end
 
   add_foreign_key "targets", "projects"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+project1 = Project.create(name: "Bob's Project",
+                         description: "This project is about Bob's Lake and surrounding ecology")
+
+Target.create(name: "Bob's Lake",
+              target_type: "lake",
+              description: "Bob's Lake is small lake outside Washington DC that is home to many unqiue and migratory birds. etc.",
+              project: project1)
+
+project2 = Project.create(name: "Emily's Project",
+                         description: "This project is about Emily's Lake and surrounding ecology")
+Target.create(name: "Emily's Lake",
+              target_type: "lake",
+              description: "Emily's Lake is small lake outside Washington DC that is home to many unqiue and migratory birds. etc.",
+              project: project2)
