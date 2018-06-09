309be30ab05f5e81f47a26a1b96341c1d8a4700c
Jamie Gaskins <jgaskins@gmail.com>
2018-06-09 09:57:04 -0400

    Add HealthRating model

---
+++ app/models/health_rating.rb
@@ -0,0 +1,2 @@
+class HealthRating < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_194433) do
+ActiveRecord::Schema.define(version: 2018_06_09_135521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "health_ratings", force: :cascade do |t|
+    t.string "rating"
+    t.string "description"
+    t.float "value"
+    t.string "weight"
+    t.string "float"
+    t.boolean "locked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
