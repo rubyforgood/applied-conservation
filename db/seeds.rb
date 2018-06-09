fdc40f998df4920f7bcf7f86e11d72b668071cf7
Jamie Gaskins <jgaskins@gmail.com>
2018-06-09 09:42:18 -0400

    Add TargetType seeds
    This commit also makes the other seeds idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+target_types = [
+  "Forest",
+  "Woodland",
+  "Savanna",
+  "Grassland",
+  "Shrubland",
+  "Steppe",
+  "Tundra",
+  "Wetland",
+  "Meadow",
+  "Riparian",
+  "River/Stream/Tributary",
+  "Lake",
+  "Springs",
+  "Estuary",
+  "Marine",
+  "Ecosystem Mosaic",
+  "Rare Plant Community or Species",
+  "Resident Bird, Fish, Mammal Species",
+  "Migratory Bird, Fish, Mammal",
+  "Keystone Species",
+  "Umbrella Species",
+  "Species Assemblage",
+  "Other",
+]
+target_types.each do |name|
+  TargetType.find_or_create_by!(name: name)
+end
 
-project1 = Project.create(name: "Bob's Project",
-                         description: "This project is about Bob's Lake and surrounding ecology")
+project1 = Project.find_or_create_by!(
+  name: "Bob's Project",
+  description: "This project is about Bob's Lake and surrounding ecology",
+)
 
-target_type = TargetType.create(name: 'Lake')
+target_type = TargetType.find_by!(name: 'Lake')
 
-Target.create(name: "Bob's Lake",
-              target_type_id: target_type.id,
-              description: "Bob's Lake is small lake outside Washington DC that is home to many unqiue and migratory birds. etc.",
-              project: project1)
+Target.find_or_create_by!(
+  name: "Bob's Lake",
+  target_type_id: target_type.id,
+  description: "Bob's Lake is small lake outside Washington DC that is home to many unqiue and migratory birds. etc.",
+  project: project1,
+)
 
-project2 = Project.create(name: "Emily's Project",
-                         description: "This project is about Emily's Lake and surrounding ecology")
-Target.create(name: "Emily's Lake",
-              target_type_id: target_type.id,
-              description: "Emily's Lake is small lake outside Washington DC that is home to many unqiue and migratory birds. etc.",
-              project: project2)
+project2 = Project.find_or_create_by!(
+  name: "Emily's Project",
+  description: "This project is about Emily's Lake and surrounding ecology",
+)
+
+Target.find_or_create_by!(
+  name: "Emily's Lake",
+  target_type_id: target_type.id,
+  description: "Emily's Lake is small lake outside Washington DC that is home to many unqiue and migratory birds. etc.",
+  project: project2,
+)
