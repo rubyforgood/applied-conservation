17f9a2705fee5706b2cda2437d03db9fcaa9c148
Jonathan Whittington <jonathantwhittington@gmail.com>
2018-06-09 13:00:06 -0400

    Add options for targets.

---
+++ config/initializers/constants.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+TARGET_OPTIONS = %w[
+  Forest
+  Woodland
+  Savanna
+  Grassland
+  Shrubland
+  Steppe
+  Tundra
+  Wetland
+  Meadow
+  Riparian
+  River/Stream/Tributary
+  Lake
+  Springs
+  Estuary
+  Marine
+  Ecosystem Mosaic
+  Rare Plant Community or Species
+  Resident Bird Fish Mammal Species
+  Migratory Bird Fish Mammal - specify the life stage
+  Keystone Species
+  Umbrella Species
+  Species Assemblage
+  Other
+].freeze
