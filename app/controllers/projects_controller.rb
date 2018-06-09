54f0e20d0e90edb5af301f8a8b3c933eb41d6c27
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2018-06-09 11:24:55 -0400

    Implement New Project creation functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,35 @@
 # frozen_string_literal: true
 
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show]
+
   def index
     @projects = Project.all
   end
 
-  def show
+  def show; end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: 'project was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_project
     @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
