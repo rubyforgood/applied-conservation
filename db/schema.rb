7236115585cfea7df09776bec7bd2e66941a8691
Brian Murphy <bmurphy1@mac.com>
2018-06-10 12:17:49 -0400

    fix tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_06_10_150004) do
-=======
 ActiveRecord::Schema.define(version: 2018_06_10_160412) do
->>>>>>> Rename Grades to Ratings app-wide
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
