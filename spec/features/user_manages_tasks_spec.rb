b7a0f114b9dcc2b0a89c2a23209b8d679d7e2a42
Jonathan Whittington <jonathantwhittington@gmail.com>
2018-06-10 10:31:52 -0400

    replace ivars with lets in user_manages_tasks_spec

--- spec/features/user_manages_tasks_spec.rb
+++ spec/features/user_manages_tasks_spec.rb
@@ -1,59 +1,53 @@
-require 'rails_helper' 
+require 'rails_helper'
 
 feature 'Task management' do
   include Warden::Test::Helpers
   Warden.test_mode!
-  task_one_name = 'TEST TASK 1'
-  task_two_name = 'TEST TASK 2'
-  user_email = 'blah@blah.com'
-
-  before do
-    @project_name = 'TEST PROJECT'
-
-    @project = Project.create(name: @project_name)
-    @task_one = Task.create(name: task_one_name, project: @project)
-    @task_two = Task.create(name: task_two_name, project: @project, user_id: '1')
-  end
+  let(:task_one_name) { 'TEST TASK 1' }
+  let(:task_two_name) { 'TEST TASK 2' }
+  let(:user_email) { 'blah@blah.com' }
+  let(:project_name) { 'TEST PROJECT' }
+  let(:project) { Project.create(name: project_name) }
+  let!(:task_one) { Task.create(name: task_one_name, project: project) }
+  let!(:task_two) { Task.create(name: task_two_name, project: project, user_id: '1') }
+  let!(:user) { User.create(email: user_email, password: 'password', password_confirmation: 'password') }
 
   scenario 'User can add a new Task' do
-    User.create(email:'blah@blah.com', password:'password', password_confirmation:'password')
-    visit "/projects/#{@project.id}"
+    visit "/projects/#{project.id}"
     click_link 'Add New Task'
 
     fill_in('Name', with: 'NEW TASK')
     fill_in('Due on', with: '04/20/2018')
-    select('blah@blah.com', from: 'task[user_id]')
+    select(user_email, from: 'task[user_id]')
     fill_in('Description', with: 'TASK DESCRIPTION')
     click_button('Add Task')
 
     expect(page).to have_text('NEW TASK')
-    expect(current_path).to eq "/projects/#{@project.id}"
+    expect(current_path).to eq "/projects/#{project.id}"
   end
 
   scenario 'User can view all tasks for a Project' do
-    visit "/projects/#{@project.id}"
+    visit "/projects/#{project.id}"
 
-    expect(page).to have_text(@project_name)
+    expect(page).to have_text(project_name)
     expect(page).to have_text(task_one_name)
     expect(page).to have_text(task_two_name)
   end
 
-  scenario "User can view a button for #{task_one_name} but not #{task_two_name}" do
-    user = User.create(email:user_email, password:'password', password_confirmation:'password')
+  scenario "User can view a button for task 1 but not task 2" do
     login_as(user, scope: :user)
-    visit "/projects/#{@project.id}"
+    visit "/projects/#{project.id}"
 
-    expect(page).to have_selector("#pickup-task-#{@task_one.id}")
-    expect(page).not_to have_selector("#pickup-task-#{@task_two.id}")
+    expect(page).to have_selector("#pickup-task-#{task_one.id}")
+    expect(page).not_to have_selector("#pickup-task-#{task_two.id}")
   end
 
-  scenario "User can pickup #{task_one_name}" do
-    user = User.create(email:'blah@blah.com', password:'password', password_confirmation:'password')
+  scenario "User can pickup task 1" do
     login_as(user, scope: :user)
-    visit "/projects/#{@project.id}"
+    visit "/projects/#{project.id}"
 
-    click_on("pickup-task-#{@task_one.id}")
-    expect(page).not_to have_selector("#pickup-task-#{@task_one.id}")
+    click_on("pickup-task-#{task_one.id}")
+    expect(page).not_to have_selector("#pickup-task-#{task_one.id}")
     expect(page).to have_text("#{task_one_name} #{user_email}")
   end
 end
