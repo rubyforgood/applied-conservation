d76036d159124b7473faa24856904813fbe6bcfd
maebeale <maebeale@gmail.com>
2018-06-19 21:53:26 -0400

    Extract import code from controller into a HealthRatingStandardsImporter model

--- app/controllers/health_rating_standards_controller.rb
+++ app/controllers/health_rating_standards_controller.rb
@@ -1,5 +1,3 @@
-require 'csv'
-
 class HealthRatingStandardsController < ApplicationController
   def import
     @health_rating_standards = HealthRatingStandard.all
@@ -8,13 +6,8 @@ class HealthRatingStandardsController < ApplicationController
   def import_csv
     uploaded_io = params[:ratings].tempfile
     path = uploaded_io.path.to_s
-    CSV.read(path, headers: true).map(&:to_h).each do |attributes|
-      target_type = TargetType.find_or_create_by!(name: attributes['target_type_name'])
-      rating = attributes['rating_name']
-      HealthRatingStandard.create!(target_type: target_type,
-                                   rating: rating,
-                                   description: attributes['description'])
-    end
+    importer = Importers::HealthRatingStandardsImporter.new(path)
+    importer.call
 
     redirect_to import_health_rating_standards_path
   end
