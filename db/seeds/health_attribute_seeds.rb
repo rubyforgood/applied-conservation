16f62fce14043c304dd0d568929a884da6022007
Eliza Aierstuck <elizaaa12@gmail.com>
2018-07-03 08:54:49 -0700

    Add terrestrial ecosystem health attribute seeds

--- db/seeds/health_attribute_seeds.rb
+++ db/seeds/health_attribute_seeds.rb
@@ -1,4 +1,5 @@
 freshwater_target_type = TargetType.find_by_name('Freshwater Ecosystem')
+terrestrial_target_type = TargetType.find_by_name('Terrestrial Ecosystem')
 
 HealthAttribute.find_or_create_by!(
   title: 'Flows',
@@ -20,3 +21,20 @@ HealthAttribute.find_or_create_by!(
   title: 'Water Quality',
   target_type: freshwater_target_type,
   description: 'dissolved oxygen, sediment, nutrients, toxins')
+
+HealthAttribute.find_or_create_by!(
+  title: 'Fire regime',
+  target_type: terrestrial_target_type,
+  description: 'frequency, type of fires, size')
+HealthAttribute.find_or_create_by!(
+  title: 'Composition',
+  target_type: terrestrial_target_type,
+  description: 'native species')
+HealthAttribute.find_or_create_by!(
+  title: 'Structure',
+  target_type: terrestrial_target_type,
+  description: 'age classes, canopy cover')
+HealthAttribute.find_or_create_by!(
+  title: 'Size',
+  target_type: terrestrial_target_type,
+  description: 'minimum dynamic area')
